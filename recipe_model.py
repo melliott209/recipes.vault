e4a8cb1ebb79457b6c49fea9e932afe2d0a411d1
melliott209 <m.elliott92@yahoo.com>
2024-06-18 21:09:24 -0600

    feat: add all and find for Recipe class

--- recipe_model.py
+++ recipe_model.py
@@ -1,14 +1,52 @@
 class Recipe:
     db = {}
 
-    def __init__(self, id, name, ingredients, instructions) -> None:
+    def __init__(
+        self,
+        name,
+        ingredients,
+        instructions,
+        id=None,
+    ) -> None:
         self.id_ = id
         self.name_ = name
         self.ingredients_ = ingredients
         self.instructions_ = instructions
+
+    def __str__(self):
+        return f"{self.id_}:{self.name_}"
 
     def id(self):
         return self.id_
 
     def name(self):
         return self.name_
+
+    def ingredients(self):
+        return self.ingredients_
+
+    def instructions(self):
+        return self.instructions_
+
+    def save(self):
+        if self.id_ is None:
+            self.id_ = len(Recipe.db)
+        Recipe.db[self.id_] = self
+        for r in Recipe.all():
+            print(r)
+
+    @classmethod
+    def clear(cls):
+        cls.db.clear()
+
+    @classmethod
+    def all(cls):
+        return list(cls.db.values())
+
+    @classmethod
+    def find(cls, query):
+        result = []
+        for r in list(cls.db.values()):
+            if r.name().find(query) >= 0:
+                result.append(r)
+        return result
--- test_recipe_model.py
+++ test_recipe_model.py
@@ -1,11 +1,50 @@
 from recipe_model import Recipe
 
 
-def test_new_recipe_id():
-    r = Recipe(1, "Blob", [], [])
-    assert r.id() == 1
+class TestRecipe:
+    def setup_method(method):
+        Recipe.clear()  # Clear the db before each test
 
+    def test_save_recipe(self):
+        r = Recipe("Test1", [], [])
+        r.save()
+        recipes = Recipe.all()
+        assert len(recipes) == 1
+        assert recipes[0].id() == 0
+        assert recipes[0].name() == "Test1"
+        assert recipes[0].ingredients() == []
+        assert recipes[0].instructions() == []
 
-def test_new_recipe_name():
-    r = Recipe(1, "Blob", [], [])
-    assert r.name() == "Blob"
+    def test_save_two_recipes(self):
+        r1 = Recipe("Test2", [], [])
+        r2 = Recipe("Test3", [], [])
+        r1.save()
+        r2.save()
+        recipes = Recipe.all()
+        assert len(recipes) == 2
+        assert recipes[0].id() == 0
+        assert recipes[0].name() == "Test2"
+        assert recipes[0].ingredients() == []
+        assert recipes[0].instructions() == []
+        assert recipes[1].id() == 1
+        assert recipes[1].name() == "Test3"
+        assert recipes[1].ingredients() == []
+        assert recipes[1].instructions() == []
+
+    def test_find_recipe_exact(self):
+        r = Recipe("sandwich", [], [])
+        r.save()
+        result = Recipe.find("sandwich")
+        assert len(result) == 1
+
+    def test_find_recipe_partial(self):
+        r = Recipe("sandwich", [], [])
+        r.save()
+        result = Recipe.find("sand")
+        assert len(result) == 1
+
+    def test_find_recipe_not_found(self):
+        r = Recipe("sandwich", [], [])
+        r.save()
+        result = Recipe.find("omelette")
+        assert len(result) == 0
